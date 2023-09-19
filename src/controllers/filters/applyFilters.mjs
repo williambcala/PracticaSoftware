d666e938123be625f4c53413a6b10844f3f08e18
williambcala <william.banguera@uao.edu.co>
2023-09-18 19:03:20 -0500

    3 commit

--- src/controllers/filters/applyFilters.mjs
+++ src/controllers/filters/applyFilters.mjs
@@ -2,6 +2,7 @@ import Process from "../../models/Process.mjs"
 import Joi from "joi";
 import Boom from "@hapi/boom";
 import { BLUR_FILTER, GREYSCALE_FILTER, NEGATIVE_FILTER } from "../../commons/constans.mjs";
+import sharp from "sharp";
 
 const PayloadValidation = Joi.object({
     filters:Joi.array().min(1).items(Joi.string().valid(
@@ -10,18 +11,51 @@ const PayloadValidation = Joi.object({
    
 })
 
-const applyFilters = async (payload) =>{
-    try{
+const applyFilters = async (payload, files) => {
+    try {
         await PayloadValidation.validateAsync(payload);
-    }catch(error){
-        throw Boom.badData(error.message,{error});
+
+        const filters = payload.filters;
+        const processedFiles = [];
+
+        for (const file of files['files[]']) {
+            const { originalname, buffer } = file;
+
+            const image = sharp(buffer);
+
+            for (const filter of filters) {
+                switch (filter) {
+                    case BLUR_FILTER:
+                        image.blur();
+                        break;
+                    case GREYSCALE_FILTER:
+                        image.greyscale();
+                        break;
+                    case NEGATIVE_FILTER:
+                        image.negate();
+                        break;
+               
+                }
+            }
+
+            // Convierte la imagen procesada de sharp a un búfer
+            const processedBuffer = await image.toBuffer();
+
+            // Guarda el archivo procesado en el arreglo
+            processedFiles.push({ originalname, buffer: processedBuffer });
+        }
+
+        const newProcess = new Process();
+        newProcess.filters = filters;
+        newProcess.processedFiles = processedFiles;
+        await newProcess.save();
+
+        return "Filtros aplicados";
+
+    } catch (error) {
+        throw Boom.badData(error.message, { error });
     }
-    const newProcess = new Process();
-    newProcess.filters = payload.filters;
+};
 
-    await newProcess.save();
+export default applyFilters;
 
-    return newProcess;
-}
-
-export default applyFilters;je
--- src/handlers/applyFiltersHandler.mjs
+++ src/handlers/applyFiltersHandler.mjs
@@ -1,17 +1,23 @@
 import applyFilters from "../controllers/filters/applyfilters.mjs";
 import Boom from "@hapi/boom";
 import HttpStatusCode from "http-status-codes";
+import express from "express";
+import multer from "multer";
 
-const applyFiltersHandler = async(req,res, next) =>{
-    try{
+const applyFiltersHandler = express.Router();
+const storage = multer.memoryStorage();//multer con memory
+const upload = multer({storage:storage});
+
+applyFiltersHandler.post("/", upload.fields([{ name: 'files[]' }, { name: 'filters' }]), async (req, res, next) => {
+    try {
         const body = req.body;
-        const response = await applyFilters(body);
+        const files = req.files;
+        const response = await applyFilters(body,files);
         return res.status(HttpStatusCode.OK).json(response);
-    }catch(error){
+    } catch (error) {
         const err = Boom.isBoom(error) ? error : Boom.internal(error);
         next(err);
-
     }
-   
-}
+});
+
 export default applyFiltersHandler;
