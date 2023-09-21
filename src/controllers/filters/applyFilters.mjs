e050a01b6a49eaf50c2482f23ddb8f88ad08cf6e
williambcala <william.banguera@uao.edu.co>
2023-09-21 18:17:08 -0500

    Continue error

--- src/controllers/filters/applyFilters.mjs
+++ src/controllers/filters/applyFilters.mjs
@@ -1,61 +1,56 @@
-import Process from "../../models/Process.mjs"
-import Joi from "joi";
-import Boom from "@hapi/boom";
-import { BLUR_FILTER, GREYSCALE_FILTER, NEGATIVE_FILTER } from "../../commons/constans.mjs";
-import sharp from "sharp";
+import Joi from 'joi';
+import Boom from '@hapi/boom';
+import sharp from 'sharp';
+import { BLUR_FILTER, GREYSCALE_FILTER, NEGATIVE_FILTER } from '../../commons/constans.mjs';
+import Process from '../../models/Process.mjs';
 
 const PayloadValidation = Joi.object({
-    filters:Joi.array().min(1).items(Joi.string().valid(
-        NEGATIVE_FILTER,GREYSCALE_FILTER,BLUR_FILTER
-    )),
-   
-})
+  filters: Joi.array().min(1).items(Joi.string().valid(NEGATIVE_FILTER, GREYSCALE_FILTER, BLUR_FILTER)),
+
+});
 
 const applyFilters = async (payload, files) => {
-    try {
-        await PayloadValidation.validateAsync(payload);
+  try {
+    await PayloadValidation.validateAsync(payload);
 
-        const filters = payload.filters;
-        const processedFiles = [];
+    const { filters } = payload;
+    const processedFiles = [];
 
-        for (const file of files['files[]']) {
-            const { originalname, buffer } = file;
+    for (const file of files['files[]']) {
+      const { originalname, buffer } = file;
 
-            const image = sharp(buffer);
+      const image = sharp(buffer);
 
-            for (const filter of filters) {
-                switch (filter) {
-                    case BLUR_FILTER:
-                        image.blur();
-                        break;
-                    case GREYSCALE_FILTER:
-                        image.greyscale();
-                        break;
-                    case NEGATIVE_FILTER:
-                        image.negate();
-                        break;
-               
-                }
-            }
-
-            // Convierte la imagen procesada de sharp a un búfer
-            const processedBuffer = await image.toBuffer();
-
-            // Guarda el archivo procesado en el arreglo
-            processedFiles.push({ originalname, buffer: processedBuffer });
+      for (const filter of filters) {
+        switch (filter) {
+          case BLUR_FILTER:
+            image.blur();
+            break;
+          case GREYSCALE_FILTER:
+            image.greyscale();
+            break;
+          case NEGATIVE_FILTER:
+            image.negate();
+            break;
         }
+      }
 
-        const newProcess = new Process();
-        newProcess.filters = filters;
-        newProcess.processedFiles = processedFiles;
-        await newProcess.save();
+      // Convierte la imagen procesada de sharp a un búfer
+      const processedBuffer = await image.toBuffer();
 
-        return "Filtros aplicados";
-
-    } catch (error) {
-        throw Boom.badData(error.message, { error });
+      // Guarda el archivo procesado en el arreglo
+      processedFiles.push({ originalname, buffer: processedBuffer });
     }
+
+    const newProcess = new Process();
+    newProcess.filters = filters;
+    newProcess.processedFiles = processedFiles;
+    await newProcess.save();
+
+    return 'Filtros aplicados';
+  } catch (error) {
+    throw Boom.badData(error.message, { error });
+  }
 };
 
 export default applyFilters;
-
