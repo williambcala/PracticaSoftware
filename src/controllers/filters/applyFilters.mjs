6b06c6a73694ea96e37d4eb57c81bb18dbe0e7bd
williambcala <william.banguera@uao.edu.co>
2023-09-26 11:14:44 -0500

    lint tru

--- src/controllers/filters/applyFilters.mjs
+++ src/controllers/filters/applyFilters.mjs
@@ -5,52 +5,31 @@ import { BLUR_FILTER, GREYSCALE_FILTER, NEGATIVE_FILTER } from '../../commons/co
 import Process from '../../models/Process.mjs';
 
 const PayloadValidation = Joi.object({
-  filters: Joi.array().min(1).items(Joi.string().valid(NEGATIVE_FILTER, GREYSCALE_FILTER, BLUR_FILTER)),
-
+  filters: Joi.array().min(1).items(Joi.string().valid(
+    NEGATIVE_FILTER,
+    GREYSCALE_FILTER,
+    BLUR_FILTER,
+  )),
 });
-
 const applyFilters = async (payload, files) => {
+  // eslint-disable-next-line
+  console.log(payload);
   try {
     await PayloadValidation.validateAsync(payload);
-
     const { filters } = payload;
-    const processedFiles = [];
-
-    for (const file of files['files[]']) {
+    const processedFiles = await Promise.all(files.map(async (file) => {
       const { originalname, buffer } = file;
-
       const image = sharp(buffer);
-
-      for (const filter of filters) {
-        switch (filter) {
-          case BLUR_FILTER:
-            image.blur();
-            break;
-          case GREYSCALE_FILTER:
-            image.greyscale();
-            break;
-          case NEGATIVE_FILTER:
-            image.negate();
-            break;
-        }
-      }
-
-      // Convierte la imagen procesada de sharp a un búfer
       const processedBuffer = await image.toBuffer();
-
-      // Guarda el archivo procesado en el arreglo
-      processedFiles.push({ originalname, buffer: processedBuffer });
-    }
-
+      return { originalname, buffer: processedBuffer };
+    }));
     const newProcess = new Process();
     newProcess.filters = filters;
     newProcess.processedFiles = processedFiles;
     await newProcess.save();
-
     return 'Filtros aplicados';
   } catch (error) {
     throw Boom.badData(error.message, { error });
   }
 };
-
 export default applyFilters;
--- src/handlers/applyFiltersHandler.mjs
+++ src/handlers/applyFiltersHandler.mjs
@@ -1,18 +1,17 @@
 import Boom from '@hapi/boom';
 import HttpStatusCode from 'http-status-codes';
 import express from 'express';
-import multer from 'multer';
+
 // eslint-disable-next-line
 import applyFilters from '../controllers/filters/applyfilters.mjs';
 
 const applyFiltersHandler = express.Router();
-const storage = multer.memoryStorage();// multer con memory
-const upload = multer({ storage });
-
-applyFiltersHandler.post('/', upload.fields([{ name: 'files[]' }, { name: 'filters' }]), async (req, res, next) => {
+// eslint-disable-next-line
+applyFiltersHandler.post('/',async(req,res,next)=>{
   try {
     const { body } = req;
     const { files } = req;
+
     const response = await applyFilters(body, files);
     return res.status(HttpStatusCode.OK).json(response);
   } catch (error) {
@@ -20,5 +19,4 @@ applyFiltersHandler.post('/', upload.fields([{ name: 'files[]' }, { name: 'filte
     next(err);
   }
 });
-
 export default applyFiltersHandler;
